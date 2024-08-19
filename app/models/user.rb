4953ec2eafcca2e5b4148516ce5372409deb151d
Tanmayi Manku <tanmayitanum@gmail.com>
2024-08-19 10:22:28 +0530

    update the model to have email, name, uid and provider columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    def self.find_or_create_from_auth_hash(auth_hash)
+        where(uid: auth_hash['uid'], provider: auth_hash['provider']).first_or_create do |user|
+          user.name = auth_hash['info']['name']
+          user.email = auth_hash['info']['email']
+          user.uid = auth_hash['uid']
+          user.provider = auth_hash['provider']
+        end
+    end
 end
